e9f8ec2733491edd7246adc6bc7094f560be6f41
DLindquist <dannyl2131@gmail.com>
2023-03-15 19:20:36 -0600

    added timer functionality, questions array, and some initial logic

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,110 @@
-let questions = []
+let startButton = document.getElementById("startButton");
+let time = document.getElementById("timeLeft");
+let firstContent = document.getElementById("content")
+let question = document.getElementById("question")
+let text = document.getElementById("text");
+let list = document.createElement("ul");
+let makeLi = document.createElement("li");
+
+let pickQuestion = function(){
+    let num = Math.floor(Math.random() * questions.length);
+    return(questions[num]);
+}
+
+let printQuestion = function (){
+    let pickedQ = pickQuestion();
+    let keys = Object.keys(pickedQ);
+    let q = Object.entries(pickedQ)
+    question.textContent = pickedQ.question;
+    text.style.display = "none";
+    startButton.style.display = "none";
+    firstContent.appendChild(list);
+    for(let i = 1; i <= 4; i++){
+        list.appendChild(makeLi);
+        makeLi.textContent = q[i][1];
+    }
+}
+
+let setTimer = function(){
+    time.textContent = "30";
+    let secondsLeft = 30;
+    let timerInterval = setInterval(function(){
+        secondsLeft--;
+        time.textContent = secondsLeft;
+        if(secondsLeft === 0){
+        clearInterval(timerInterval);
+    }
+    }, 1000)
+}
+
+startButton.addEventListener("click", function(){
+    setTimer()
+    pickQuestion()
+    printQuestion()
+});
+
+let questions = [
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    },
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    },
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    },
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    },
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    },
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    },
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    },
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    },
+    {
+        "question": "what are you talking about?",
+        "answer1": "sample",
+        "answer2": "sample",
+        "answer3": "sample",
+        "answer4": "sample",
+    }
+]
